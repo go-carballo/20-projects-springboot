fff05d8933aa6c97cf144107f15196b1cd5f3209
Alejandro Carballo <o.alejandro.carballo@gmail.com>
2025-12-16 14:44:48 -0300

    Modifed enpoints structure

--- 01-gestor-notas-personales/working/src/main/java/com/payoyo/gestor_notas_personales/repository/NoteRepository.java
+++ 01-gestor-notas-personales/working/src/main/java/com/payoyo/gestor_notas_personales/repository/NoteRepository.java
@@ -2,6 +2,8 @@ package com.payoyo.gestor_notas_personales.repository;
 
 import com.payoyo.gestor_notas_personales.entity.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
 }
